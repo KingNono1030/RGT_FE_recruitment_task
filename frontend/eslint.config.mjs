cde0c769a4b452aa05c42c85e9213a607b64ca4d
KingNono1030 <hoejy0821@gmail.com>
2024-12-12 18:11:25 +0900

    feat: add frontend-specific configuration and initial setup

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,10 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+export default [
+  ...compat.extends([
+    'next/core-web-vitals', // Next.js 권장 규칙
+    'next/typescript', // Next.js 타입스크립트 규칙
+  ]),
+]
