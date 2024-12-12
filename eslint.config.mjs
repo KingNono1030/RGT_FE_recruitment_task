2072fcd4001544a96e262751a71fde0c2b3961c3
KingNono1030 <hoejy0821@gmail.com>
2024-12-12 18:15:41 +0900

    choer: add common lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  ...compat.extends([
+    'eslint:recommended', // 공통 권장 규칙
+    'plugin:@typescript-eslint/recommended', // 타입스크립트 권장 규칙
+    'prettier', // Prettier와 통합
+  ]),
+  {
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
+  },
+]
