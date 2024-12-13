9eaf04a8cdd10b19919f98e26ae9b566a6451985
KingNono1030 <hoejy0821@gmail.com>
2024-12-13 20:41:11 +0900

    chore: rewrite error message in kr

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,14 +3,14 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
   const MONGO_URI = process.env.MONGO_URI || ''
   if (!MONGO_URI) {
-    throw new Error('MongoDB URI is not defined in environment variables.')
+    throw new Error('MongoDB URI 가 환경 변수에서 찾을 수 없습니다.')
   }
   try {
     mongoose.set('strictQuery', true)
     await mongoose.connect(MONGO_URI)
-    console.log('Connected to MongoDB')
+    console.log('MongoDB 에 연결했습니다.')
   } catch (error) {
-    console.error('MongoDB connection error:', error)
+    console.error('MongoDB 연결 에러:', error)
     process.exit(1)
   }
 }
