eaa52434564605d08b8bd345d8169327108b44ed
KingNono1030 <hoejy0821@gmail.com>
2024-12-14 23:45:01 +0900

    feat: add _id props to book type

--- frontend/src/types/Book.types.d.ts
+++ frontend/src/types/Book.types.d.ts
@@ -1,7 +1,14 @@
-export type Book = {
+export type BookBase = {
   title: string
   author: string
   publishedDate: string
   genre: string
   summary?: string
 }
+
+export interface BookListItem extends BookBase {
+  _id: string
+}
+
+export type CreateBookRequest = BookBase
+export type UpdateBookRequest = BookBase
