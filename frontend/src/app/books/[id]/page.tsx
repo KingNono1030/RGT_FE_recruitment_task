acabe61eff2831c833689a70b16be3ea14d80296
KingNono1030 <hoejy0821@gmail.com>
2024-12-14 23:47:50 +0900

    feat: add book detail page

--- frontend/src/app/books/[id]/page.tsx
+++ frontend/src/app/books/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { getBookById } from '@/services/bookServices'
+import type { BookListItem } from '@/types/Book.types'
 interface BookDetailPageProps {
   params: {
     id: string
@@ -6,11 +8,33 @@ interface BookDetailPageProps {
 
 export default async function BookDetailPage({ params }: BookDetailPageProps) {
   const { id } = await params
-
+  const data = await getBookById(id)
+  const book: BookListItem = data
   return (
-    <div>
-      <h1>책 상세</h1>
-      <p>Book ID: {id}</p>
+    <div className='flex flex-col md:flex-row gap-4 xl:gap-4 p-4 md:p-6'>
+      <div className='w-full min-h-20 md:w-[340px] md:h-[340px] xl:w-[486px] xl:h-[486px] rounded-xl md:rounded-2xl bg-slate-300'>
+        이미지
+      </div>
+      <div className='flex-grow'>
+        <div className='pb-4 border-solid border-b-2 border-gray-800 mb-6'>
+          <h1 className='text-2xl mb-4 font-semibold'>{book.title}</h1>
+          <h2 className='text-4xl font-semibold'>{book.author}</h2>
+        </div>
+        <div>
+          <div className='mb-2'>
+            <h3 className='font-semibold'>책 소개</h3>
+            <p>{book.summary}</p>
+          </div>
+          <div className='mb-2'>
+            <h3 className='font-semibold'>장르</h3>
+            <p>{book.genre}</p>
+          </div>
+          <div>
+            <h3 className='font-semibold'>발행일</h3>
+            <p>{book.publishedDate}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
